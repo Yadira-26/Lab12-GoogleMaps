668da4b1ba7150574877eda60a9c0716e145ce03
Yadira-26 <yadirazeballosm@gmail.com>
2025-06-09 19:38:09 -0500

    Agregar marcador personalizado con ícono en MapScreen

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
@@ -30,6 +31,7 @@ fun MapScreen() {
             // Añadir marcador en Denver, Colorado
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
+                icon = BitmapDescriptorFactory.fromResource(R.drawable.mountain_icon),
                 title = "Arequipa, Perú"
             )
         }
